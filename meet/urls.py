ca7438c58c3ea7c24d47f56b333f727d4d4252b5
Naitik <naitik12gupta05@gmail.com>
2024-11-27 01:14:23 +0530

    changed password-reset view and set new passwd views

--- meet/urls.py
+++ meet/urls.py
@@ -4,6 +4,6 @@ from .views import RegisterView, LoginView, PasswordResetRequestView,SetNewPassw
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='login'),
-    path('password-reset-request', PasswordResetRequestView.as_view(), name='password_reset_request'),
+    path('password-reset-request/', PasswordResetRequestView.as_view(), name='password_reset_request'),
     path('set-new-password/', SetNewPasswordView.as_view(), name='set_new_password'),
 ]
